14947425318d8e7d1202aa6c191151a9df174ca1
Murtaza-Ali-Asghar <156521197+Murtaza-Ali-Asghar@users.noreply.github.com>
2024-02-10 16:16:19 +0500

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,15 @@
+
+var age = 20;
+var isStudent = true;
+
+console.log('Murtaza');
+console.log(age);
+console.log(isStudent);
+
+
+var greet = ('Hello Murtaza!');
+var introduction = ("Welcome! We are glad you're here.");
+
+var WelcomeMessage = (greet + '\n' + introduction);
+
+alert(WelcomeMessage);
