aad14054363f44a7274963522e61f7d7b4023f5b
Murtaza-Ali-Asghar <156521197+Murtaza-Ali-Asghar@users.noreply.github.com>
2024-02-10 17:07:09 +0500

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,13 @@
+
+let length = 50;
+let width = 25;
+
+let area = length + width;
+
+console.log('The area of the rectangle is:' + area);
+
+
+
+let userName = prompt('Please enter your name:');
+
+alert('Hello' + userName + '!' + '\n' + 'Welcome to this Website.');
